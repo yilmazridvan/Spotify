c6e827463cda5f9e1a02e34601baa2d3785ea1f8
Rıdvan Yılmaz <110398656+yilmazridvan@users.noreply.github.com>
2023-02-27 14:42:29 +0300

    UI fixes

--- Spotify/Controllers/Core/HomeViewController.swift
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -8,13 +8,63 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+            return HomeViewController.createSectionlayout(section: sectionIndex)
+        }
+    )
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.tintColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Browse"
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
+        view.addSubview(spinner)
+        configureCollectionView()
         fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private static func createSectionlayout(section: Int) -> NSCollectionLayoutSection {
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        // Group
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(120)),
+            repeatingSubitem: item,
+            count: 1
+        )
+        //Section
+        let section = NSCollectionLayoutSection(group: group)
+        return section
     }
     
     @objc private func didTapSettings() {
@@ -25,6 +75,10 @@ class HomeViewController: UIViewController {
     }
     
     private func fetchData() {
+        // Featured Playlists
+        // Recommended Tracks
+        // New Releases
+        
 //        APICaller.shared.getNewReleases { result in
 //            switch result {
 //            case .success(let model): break
@@ -37,12 +91,13 @@ class HomeViewController: UIViewController {
 //            case .failure(let error): break
 //            }
 //        }
+        
         APICaller.shared.getRecommendedGenres { result in
             switch result {
             case .success(let model):
                 let genres = model.genres
                 var seeds = Set<String>()
-                while seeds.count < 5 {
+                while seeds.count <= 5 {
                     if let random = genres.randomElement() {
                         seeds.insert(random)
                     }
@@ -56,7 +111,22 @@ class HomeViewController: UIViewController {
             }
         }
     }
-
-
 }
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    
+}
+                                        
+                                        
+                                    
 
--- Spotify/Managers/APICaller.swift
+++ Spotify/Managers/APICaller.swift
@@ -82,7 +82,7 @@ final class APICaller {
     public func getRecommendations(genres: Set<String>, completion: @escaping ((Result<RecommendationsResponse, Error>) -> Void)) {
         let seeds = genres.joined(separator: ",")
         createRequest(
-            with: URL(string: Constants.baseAPIURL + "/recommendations?seed_genres=\(seeds)"),
+            with: URL(string: Constants.baseAPIURL + "/recommendations?limit=2&seed_genres=\(seeds)"),
             type: .GET
         ) { request in
             let task = URLSession.shared.dataTask(with: request) { data, _, error in
